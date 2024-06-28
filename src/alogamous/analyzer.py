6f23a38bfaa274a25a3f300e338f6dd389e79cef
Ben Rady <benrady@gmail.com>
2024-06-28 15:19:42 -0500

    Introducing Analyzer (#32)
    * Adding Analyzer class and main function
    
    * Adding EchoAnalyzer
    
    * Adding abstractmethod annotation to Analyzer methods
    
    ---------
    
    Co-authored-by: Ben Rady <ben.rady@aquatic.com>

---
+++ src/alogamous/analyzer.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Analyzer(ABC):
+    @abstractmethod
+    def read_log_line(self, line):
+        pass
+
+    @abstractmethod
+    def report(self, out_stream):
+        pass
+
+
+def analyze_log_stream(analyzers, in_stream, out_stream):
+    for line in in_stream:
+        for analyzer in analyzers:
+            analyzer.read_log_line(line.rstrip())
+    for analyzer in analyzers:
+        analyzer.report(out_stream)
