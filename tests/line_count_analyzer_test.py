ec64191060324ea263840f36e72dca0a4eb90406
krady <krady@krady.aq.tc>
2024-07-02 15:28:28 -0500

    improving tests and deleting extra files

--- tests/line_count_analyzer_test.py
+++ tests/line_count_analyzer_test.py
@@ -1,11 +1,20 @@
 import io
 
-from alogamous import analyzer, line_count_analyzer
+from alogamous.line_count_analyzer import LineCountAnalyzer
 
 
-def test_line_count():
-    in_stream = io.StringIO("""line1
-        line2""")
+def test_report():
+    log_line_counter = LineCountAnalyzer()
     out_stream = io.StringIO()
-    analyzer.analyze_log_stream([line_count_analyzer.LineCountAnalyzer()], in_stream, out_stream)
+
+    log_line_counter.read_log_line(log_line_counter)
+    log_line_counter.read_log_line(log_line_counter)
+    log_line_counter.report(out_stream)
     assert out_stream.getvalue() == "Number of log lines: 2"
+
+
+def test_report_without_readline():
+    log_line_counter = LineCountAnalyzer()
+    out_stream = io.StringIO()
+    log_line_counter.report(out_stream)
+    assert out_stream.getvalue() == "Number of log lines: 0"
