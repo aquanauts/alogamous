8a110d0d8398b99840a0d4d34cbf149973670821
atong <atong@atong-mac.aq.tc>
2024-07-03 14:35:05 -0500

    getting rid of the fake code

--- src/alogamous/error_counter_analyzer.py
+++ src/alogamous/error_counter_analyzer.py
@@ -3,11 +3,9 @@ from alogamous import analyzer
 
 class ErrorCounterAnalyzer(analyzer.Analyzer):
     def __init__(self):
-        self.lines = []
         self.error_number = 0
 
     def read_log_line(self, line):
-        self.lines.append(line)
         if "ERROR" in line:
             self.error_number += 1
 
