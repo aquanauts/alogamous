6608be4a53d62aa701c5b420304c213754835034
Lauren Assour <lauren.assour@aquatic.com>
2024-07-09 10:28:41 -0500

    Add default TZ.

--- src/alogamous/log_line_utility.py
+++ src/alogamous/log_line_utility.py
@@ -8,5 +8,7 @@ class LogLineUtility:
         return text.split(splitter)
 
     @staticmethod
-    def parse_date(text, dt_format):
-        return datetime.datetime.strptime(text, dt_format)
+    def parse_date(text, dt_format, tz = None):
+        if not tz:
+            tz = datetime.timezone.utc
+        return datetime.datetime.strptime(text, dt_format).replace(tzinfo=tz)
