edab841ebb99d1daa494c4921dab114decf27382
Lauren Assour <lauren.assour@aquatic.com>
2024-07-09 10:29:01 -0500

    Format hatch.

--- src/alogamous/log_line_utility.py
+++ src/alogamous/log_line_utility.py
@@ -2,13 +2,12 @@ import datetime
 
 
 class LogLineUtility:
-
     @staticmethod
     def tokenize(text, splitter):
         return text.split(splitter)
 
     @staticmethod
-    def parse_date(text, dt_format, tz = None):
+    def parse_date(text, dt_format, tz=None):
         if not tz:
             tz = datetime.timezone.utc
         return datetime.datetime.strptime(text, dt_format).replace(tzinfo=tz)
