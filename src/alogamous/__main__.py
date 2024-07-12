9a295153d29b38c88dd6c13eb23a21dee79b0ae6
Lauren Assour <66278181+laurenassour@users.noreply.github.com>
2024-07-12 13:49:11 -0500

    Update __main__.py
    Lauren messed things up again

--- src/alogamous/__main__.py
+++ src/alogamous/__main__.py
@@ -1,6 +1,6 @@
 import sys
 
-from alogamous import analyzer, echo_analyzer, line_count_analyzer
+from alogamous import analyzer, echo_analyzer, error_counter_analyzer, line_count_analyzer
 
 with open("../../data/ex_log_01.txt") as log_file, open("../../data/test_output_file.txt", "a") as output_file:
     analyzer.analyze_log_stream(
