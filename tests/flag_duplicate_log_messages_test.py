7fb10a07bd6feb6f8cc4de14f24c8817203b0280
Ayesha Ahmed <ayeshaahmed@Ayeshas-MacBook-Air-2.local>
2024-07-12 13:16:45 -0500

    flag duplicate

--- tests/flag_duplicate_log_messages_test.py
+++ tests/flag_duplicate_log_messages_test.py
@@ -11,6 +11,6 @@ def test_flag_duplicate_log_messages():
     analyzer.analyze_log_stream([flag_duplicate_log_messages.FlagDuplicateLogMessages()], in_stream, out_stream)
     assert (
         out_stream.getvalue()
-        == """ Duplicate log messages:
+        == """Duplicate Log Messages:
         log message 1"""
     )
