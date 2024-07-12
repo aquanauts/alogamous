015d1cde87e6334147225d2c99450421d991e54b
Lauren Assour <66278181+laurenassour@users.noreply.github.com>
2024-07-12 13:52:27 -0500

    Update __main__.py

--- src/alogamous/__main__.py
+++ src/alogamous/__main__.py
@@ -1,10 +1,11 @@
-import sys
-
 from alogamous import analyzer, echo_analyzer, error_counter_analyzer, line_count_analyzer
 
 with open("../../data/ex_log_01.txt") as log_file, open("../../data/test_output_file.txt", "a") as output_file:
     analyzer.analyze_log_stream(
-        [echo_analyzer.EchoAnalyzer(), 
-         error_counter_analyzer.ErrorCounterAnalyzer(),
-         line_count_analyzer.LineCountAnalyzer()], log_file, output_file
+        [
+            echo_analyzer.EchoAnalyzer(), 
+            error_counter_analyzer.ErrorCounterAnalyzer(),
+            line_count_analyzer.LineCountAnalyzer()
+        ], log_file, output_file
     )
+    
