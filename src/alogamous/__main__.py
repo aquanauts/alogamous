4b6662a10eaa533b57974f0c92c4dccb7bdc25c7
kitrady <119824796+kitrady@users.noreply.github.com>
2024-07-12 14:04:13 -0500

    Adding input and output files (#40)

--- src/alogamous/__main__.py
+++ src/alogamous/__main__.py
@@ -1,13 +1,12 @@
-import sys
-
 from alogamous import analyzer, echo_analyzer, error_counter_analyzer, line_count_analyzer
 
-analyzer.analyze_log_stream(
-    [
-        echo_analyzer.EchoAnalyzer(),
-        error_counter_analyzer.ErrorCounterAnalyzer(),
-        line_count_analyzer.LineCountAnalyzer(),
-    ],
-    sys.stdin,
-    sys.stdout,
-)
+with open("../../data/ex_log_01.txt") as log_file, open("../../data/test_output_file.txt", "a") as output_file:
+    analyzer.analyze_log_stream(
+        [
+            echo_analyzer.EchoAnalyzer(),
+            error_counter_analyzer.ErrorCounterAnalyzer(),
+            line_count_analyzer.LineCountAnalyzer(),
+        ],
+        log_file,
+        output_file,
+    )
