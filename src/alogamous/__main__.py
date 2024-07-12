99803d295cd31d6ca8e52247f4f4da08912e12a9
krady <krady@krady.aq.tc>
2024-07-12 15:07:28 -0500

    integrating directory reader into main file

--- src/alogamous/__main__.py
+++ src/alogamous/__main__.py
@@ -1,12 +1,13 @@
-from alogamous import analyzer, echo_analyzer, error_counter_analyzer, line_count_analyzer
+from alogamous import analyzer, directory_reader, echo_analyzer, error_counter_analyzer, line_count_analyzer
 
-with open("../../data/ex_log_01.txt") as log_file, open("../../data/test_output_file.txt", "a") as output_file:
+with open("../../data/test_output_file.txt", "a") as output_file:
+    reader = directory_reader.DirectoryReader("../../data")
     analyzer.analyze_log_stream(
         [
             echo_analyzer.EchoAnalyzer(),
             error_counter_analyzer.ErrorCounterAnalyzer(),
             line_count_analyzer.LineCountAnalyzer(),
         ],
-        log_file,
+        reader.read(),
         output_file,
     )
