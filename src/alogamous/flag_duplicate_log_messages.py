ab5a7aaa6b32985afd22b3fe7cb9b5534d6e00bd
Ayesha Ahmed <ayeshaahmed@Ayeshas-MacBook-Air-2.local>
2024-07-19 15:03:40 -0500

    flag duplicates

--- src/alogamous/flag_duplicate_log_messages.py
+++ src/alogamous/flag_duplicate_log_messages.py
@@ -3,18 +3,20 @@ from alogamous import analyzer
 
 class FlagDuplicateLogMessages(analyzer.Analyzer):
     def __init__(self):
-        self.logMessages = []
+        self.logMessages = set()
         self.duplicateMessages = set()
 
     def read_log_line(self, line):
         message = line.split("-")[-1].strip()
-        self.logMessages.append(message)
-
-        for message in self.logMessages:
-            if self.logMessages.count(message) > 1:
-                self.duplicateMessages.add(message)
+        if message in self.logMessages:
+            self.duplicateMessages.add(message)
+        else:
+            self.logMessages.add(message)
 
     def report(self, out_stream):
-        out_stream.write("Duplicate Log Messages:\n")
-        for message in self.duplicateMessages:
-            out_stream.write(f"{message}\n")
+        if len(self.duplicateMessages) > 0:
+            out_stream.write("Duplicate Log Messages:\n")
+            for message in self.duplicateMessages:
+                out_stream.write(f"{message}\n")
+        else:
+            out_stream.write("No duplicate log messages\n")
