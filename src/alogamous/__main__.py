2d63ea45e63bd7962b459ed48d9133fd471e00d5
lozturk <lozturk@aahmed-mac.aq.tc>
2024-07-19 14:14:50 -0500

    Adding to main

--- src/alogamous/__main__.py
+++ src/alogamous/__main__.py
@@ -1,7 +1,9 @@
 import sys
 
-from alogamous import analyzer, echo_analyzer, line_count_analyzer
+from alogamous import analyzer, echo_analyzer, line_count_analyzer, loginfo_analyzer
 
 analyzer.analyze_log_stream(
-    [echo_analyzer.EchoAnalyzer(), line_count_analyzer.LineCountAnalyzer()], sys.stdin, sys.stdout
+    [echo_analyzer.EchoAnalyzer(), line_count_analyzer.LineCountAnalyzer(), loginfo_analyzer.InfoAnalyzer()],
+    sys.stdin,
+    sys.stdout,
 )
