fc4ddfbb6d05ba2f4f6c451c6ceb3db55fbe75ac
kitrady <kitprady@gmail.com>
2024-07-27 20:24:01 -0500

    improving report format of format analyzer

--- tests/format_analyzer_test.py
+++ tests/format_analyzer_test.py
@@ -3,7 +3,7 @@ from io import StringIO
 from alogamous import format_analyzer, log_line_parser
 
 
-def test_format_analyzer():
+def test_format_analyzer_with_bad_lines():
     parser = log_line_parser.LogLineParser(
         ["datetime", "source", "level", "message"], " - ", "===================================================="
     )
@@ -31,3 +31,25 @@ Lines that do not conform to log format:
 - Hello I am a bad log line
 - Hello I am a bad log line"""
     )
+
+
+def test_format_analyzer_with_good_lines():
+    parser = log_line_parser.LogLineParser(
+        ["datetime", "source", "level", "message"], " - ", "===================================================="
+    )
+    format_checker = format_analyzer.FormatAnalyzer(parser)
+    in_stream = """====================================================
+    STARTING Tracking service
+        Start time: 2024-06-20 09:00:00.001550+00:00
+        Version: 2729a
+        Command line: ['.venv/bin/python3', '-m', 'app.tracking_service', '--market', 'US', '--version', '2729a']
+    ====================================================
+    2024-06-20 11:00:17,983 - root - INFO - Adding subscription for pid None
+    2024-06-20 11:00:18,115 - root - INFO - Initialized Influx DB Client to host
+    2024-06-20 11:00:18,115 - root - INFO - Scheduling Error Handler in 150.0 seconds
+    2024-06-20 11:00:18,116 - root - INFO - prometheus client http server running"""
+    out_stream = StringIO()
+    for line in in_stream.splitlines():
+        format_checker.read_log_line(line)
+    format_checker.report(out_stream)
+    assert out_stream.getvalue() == "All lines conform to log line format"
