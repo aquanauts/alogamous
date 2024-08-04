14ba0c74a271a908c6f2cb1f54923bbd0cbbc465
kitrady <kitprady@gmail.com>
2024-08-04 13:07:50 -0500

    fixing bugs with duplicate message analyzer

--- src/alogamous/__main__.py
+++ src/alogamous/__main__.py
@@ -41,7 +41,7 @@ with open(f"{sys.argv[3]}", "a") as output_file:
             # echo_analyzer.EchoAnalyzer(),
             daily_count_analyzer.DailyCountAnalyzer(line_parser),
             error_counter_analyzer.ErrorCounterAnalyzer(),
-            flag_duplicate_log_messages.FlagDuplicateLogMessages(),
+            flag_duplicate_log_messages.FlagDuplicateLogMessages(line_parser),
             line_count_analyzer.LineCountAnalyzer(),
             format_analyzer.FormatAnalyzer(line_parser),
             warning_analyzer.WarningAnalyzer(),
